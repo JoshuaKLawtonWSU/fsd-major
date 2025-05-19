3da967e2f66dd6beca317bcbeb50f23c0a16ad8c
JoshuaKLawtonWSU <22066909@student.westernsydney.edu.au>
2025-05-19 10:21:18 +1000

    Prisma Client and minor DB additions

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,11 @@ import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 import { greet } from "@repo/utils/messages";
 import { AppLayout } from "../components/layout/appLayout";
-import type { Product } from "@repo/utils/data";
-import { products } from "@repo/utils/tempData";
+// import type { Product } from "@repo/utils/data";
+// import { products } from "@repo/utils/tempData";
 import { ProductList } from "../components/products/productList";
+// import { PrismaClient } from "@prisma/client";
+import createClient from "../../../packages/db/prismaClient";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -23,10 +25,19 @@ const ThemeImage = (props: Props) => {
   );
 };
 
+const prisma = createClient();
+async function getProducts() {
+  const products = await prisma.product.findMany();
+  return products;
+}
+const allProducts = await getProducts();
+console.log(allProducts);
+console.log("Hello console?");
+
 export default function Home() {
   return (
     <AppLayout>
-      <ProductList products={products} />
+      {/* <ProductList products={getProducts} /> */}
     </AppLayout>
   );
 
